043be59dbc1b1faff51e147e1e8cd732fe9e4429
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-26 16:19:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,14 +57,16 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+		 return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
 
 	programmer_hash =
  		{
